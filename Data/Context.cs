7d82dba459c80b690c6ede1921117fee2e0a96c1
mthsps <dmatheusps@gmail.com>
2022-11-25 19:38:18 -0300

    Add project files.

---
+++ Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using LoginApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginApp.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LoginApp.Model
+{
+    public class User
+    {
+
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
