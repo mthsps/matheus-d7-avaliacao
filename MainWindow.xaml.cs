976ae16fee8ba8e88fac0a6a4614d78593f570b6
mthsps <dmatheusps@gmail.com>
2022-11-25 21:49:36 -0300

    Created user interface

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,35 @@ namespace LoginApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string text = "Usuário autenticado!";
+
+            CreatePopup(text);
+        }
+
+        private void CreatePopup(string text) {
+
+            Popup codePopup = new Popup();
+            TextBlock popupText = new TextBlock();
+            popupText.Text = text;
+            popupText.Background = Brushes.White;
+            popupText.Height = ActualHeight;
+            popupText.Width = ActualWidth;
+            popupText.HorizontalAlignment = HorizontalAlignment.Center;
+            popupText.VerticalAlignment = VerticalAlignment.Center;
+            popupText.FontSize = 24;
+            codePopup.Child = popupText;
+            codePopup.PlacementTarget = this;
+            codePopup.Placement = PlacementMode.Center;
+            codePopup.IsOpen = true;
+            codePopup.Width = 300;
+            codePopup.Height = 180;
+
+
         }
     }
 }
